f779840ad857d9aa31edda193b3cd93836370242
natanael.apaula <natanael.apaula@CAR0588301W10-1>
2023-08-24 17:03:15 -0300

    Adicionar arquivos de projeto.

---
+++ FormFuncionarPeloAmorDeDeus/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace FormFuncionarPeloAmorDeDeus
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.painel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.btnInicio);
+            this.panel1.Location = new System.Drawing.Point(25, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 389);
+            this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(39, 159);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Produtos";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(39, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Contato";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(39, 76);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(125, 23);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(39, 38);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(125, 23);
+            this.btnInicio.TabIndex = 0;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // painel
+            // 
+            this.painel.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.painel.Location = new System.Drawing.Point(250, 36);
+            this.painel.Name = "painel";
+            this.painel.Size = new System.Drawing.Size(519, 389);
+            this.painel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.painel);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button4;
+        private Button button3;
+        private Button btnLogin;
+        private Button btnInicio;
+        private Panel painel;
+    }
+}
---
+++ FormFuncionarPeloAmorDeDeus/Form1.cs
@@ -0,0 +1,35 @@
+namespace FormFuncionarPeloAmorDeDeus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void CarregaForm(Form form )
+        {
+            form.TopLevel = false;
+            painel.Controls.Clear();
+            painel.Controls.Add( form );
+            form.Location = new Point(painel.Width/2-form.Width/2,painel.Height/2 -form.Height/2);
+            form.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregaForm(new Inicio());
+            
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            CarregaForm(new Login());
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            CarregaForm(new Inicio());
+        }
+    }
+}
